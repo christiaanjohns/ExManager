5ff00826f4c1b59ea875c81097e4537b7ae00dda
christiaanjohns <christiaanjohns@gmail.com>
2018-12-18 12:01:42 -0700

    Added Sell method and others.

--- ExManager/PortfolioTest.cpp
+++ ExManager/PortfolioTest.cpp
@@ -2,26 +2,64 @@
 #include "Portfolio.h"
 
 using namespace ::testing;
+using namespace std;
 
 class APortfolio: public Test {
 public:
+    static const string IBM;
+    static const string SAMSUNG;
     Portfolio portfolio_;
 };
 
-TEST_F(APortfolio, IsEmptyWhenCreated) {
+const string APortfolio::IBM("IBM");
+const string APortfolio::SAMSUNG("SSNLF");
+
+
+TEST_F(APortfolio, IsEmptyWhenCreated)
+{
     ASSERT_TRUE(portfolio_.IsEmpty());
 }
-
-TEST_F(APortfolio, IsNotEmptyAfterPurchase) {
-    portfolio_.Purchase("IBM", 1);
+TEST_F(APortfolio, IsNotEmptyAfterPurchase)
+{
+    portfolio_.Purchase(IBM, 1);
 
     ASSERT_FALSE(portfolio_.IsEmpty());
 }
-TEST_F(APortfolio, AnswersZeroForShareCountOfUnpurchasedSymbol) {
+TEST_F(APortfolio, AnswersZeroForShareCountOfUnpurchasedSymbol)
+{
     ASSERT_THAT(portfolio_.ShareCount("AAPL"), Eq(0u));
 }
+TEST_F(APortfolio, AnswersShareCountForPurchasedSymbol)
+{
+    portfolio_.Purchase(IBM, 2);
+    ASSERT_THAT(portfolio_.ShareCount(IBM), Eq(2u));
+}
+TEST_F(APortfolio, ThrowOnPurchaseOfZeroShares)
+{
+    ASSERT_THROW(portfolio_.Purchase(IBM, 0), InvalidPurchaseException);
+}
+TEST_F(APortfolio, AnswersShareCountForAppropriateSymbol)
+{
+    portfolio_.Purchase(IBM, 5);
+    portfolio_.Purchase(SAMSUNG, 10);
 
-TEST_F(APortfolio, AnswersShareCountForPurchasedSymbol) {
-    portfolio_.Purchase("IBM", 2);
-    ASSERT_THAT(portfolio_.ShareCount("IBM"), Eq(2u));
+    ASSERT_THAT(portfolio_.ShareCount(IBM), Eq(5u));
+}
+TEST_F(APortfolio, ShareCountReflectsAccumulatedPurchaseOfSameSymbol)
+{
+    portfolio_.Purchase(IBM, 5);
+    portfolio_.Purchase(IBM, 10);
+
+    ASSERT_THAT(portfolio_.ShareCount(IBM), Eq(5u + 10));
+}
+TEST_F(APortfolio, ReduceShareCountOfSymbolsOnSell)
+{
+    portfolio_.Purchase(SAMSUNG, 30);
+    portfolio_.Sell(SAMSUNG, 13);
+
+    ASSERT_THAT(portfolio_.ShareCount(SAMSUNG), Eq(30u-13));
+}
+TEST_F(APortfolio, ThrowsWhenSellingMoreSharesThanPurchased)
+{
+    ASSERT_THROW(portfolio_.Sell(SAMSUNG,1), InvalidSellException);
 }
